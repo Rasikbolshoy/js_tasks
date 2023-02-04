3f9895c8dde672f2265c164bc9c16aa3eaee02ef
Пользователь <it007722007@gmail.com>
2023-02-04 16:55:03 +0600

    added file 02-02

---
+++ 02-02/script.js
@@ -0,0 +1,18 @@
+/*********** 1 *********/
+const array = [10, 15, 9, 10];
+const result = array[0] * array[1] + array[2] * array[3];
+console.log(result);
+
+/********** 2 ***********/
+//const arr = ['a', 'b', 'c', 'd'];
+//console.log(`${arr[0]}+${arr[1]}, ${arr[2]}+${arr[3]}`);
+
+/******** 3 *********/
+/*
+const test = true;
+if (test === true) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+*/
