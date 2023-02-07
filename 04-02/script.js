4a016a2f7b1d8d071c5b4ed004631183ec33b556
Пользователь <it007722007@gmail.com>
2023-02-07 17:56:32 +0600

    added file 04-02

---
+++ 04-02/script.js
@@ -0,0 +1,29 @@
+/********** 1 **********/
+/*const arr = [1, 2, 5, 6, 88, 5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum);*/
+
+/************** 2 ***************/
+/*
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] * arr[i];
+}
+console.log(sum);
+*/
+
+/********* 3 *****************/
+/*const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let evenNumbers = [];
+let index = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        evenNumbers[index] = arr[i];
+        index++;
+    }
+}
+console.log(evenNumbers);*/
