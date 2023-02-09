12327bbbfa73230f0cc1a9796a6253264008e76a
Пользователь <it007722007@gmail.com>
2023-02-09 18:08:11 +0600

    added file 07-02

---
+++ 07-02/script.js
@@ -0,0 +1,40 @@
+/********** 1 **********/
+/*function calculateCube(number) {
+    return number * number * number;
+}
+
+const result = calculateCube(5);
+console.log("The cube of 5 is " + result);*/
+
+
+/********* 2 **********/
+/*
+function getName(name) {
+    if (name === undefined) {
+        console.log("Hello, guest");
+    } else {
+        console.log("Hello, " + name);
+    }
+}
+
+
+getName("John");
+getName();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
