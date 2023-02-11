444b7255f63605d9e014ef5bab96bfceec0e9c07
Пользователь <it007722007@gmail.com>
2023-02-11 18:03:23 +0600

    added file 09-02

---
+++ 09-02/script.js
@@ -0,0 +1,33 @@
+/********** 1 ************/
+/*function doubleArray(arr) {
+    return arr.map(num => num * 2);
+}
+doubleArray([1, 2, 3]); // [2, 4, 6]
+doubleArray([4, 1, 1, 1, 4]); // [8, 2, 2, 2, 8]
+doubleArray([2, 2, 2, 2, 2, 2]); // [4, 4, 4, 4, 4, 4]
+
+console.log(doubleArray([1, 2, 3])); // [2, 4, 6]
+console.log(doubleArray([4, 1, 1, 1, 4])); // [8, 2, 2, 2, 8]
+console.log(doubleArray([2, 2, 2, 2, 2, 2])); // [4, 4, 4, 4, 4, 4]*/
+
+
+/******** 2 *********/
+/*function fillArrayWithValue(value, size) {
+    return Array(size).fill(value);
+}
+fillArrayWithValue('a', 3); // ['a', 'a', 'a']
+fillArrayWithValue(0, 5); // [0, 0, 0, 0, 0]
+fillArrayWithValue(true, 2); // [true, true]
+
+console.log(fillArrayWithValue('a', 3)); // ['a', 'a', 'a']
+console.log(fillArrayWithValue(0, 5)); // [0, 0, 0, 0, 0]
+console.log(fillArrayWithValue(true, 2)); // [true, true]*/
+
+
+/********** 3 ***********/
+/*function reverseArray(arr) {
+    return arr.reverse();
+}
+const data = [1, 2, 3];
+const reversed = reverseArray(data);
+console.log(reversed); // [3, 2, 1]*/
