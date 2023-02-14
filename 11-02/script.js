267370944264eb5af1b5b491bfb801f863342baa
Пользователь <it007722007@gmail.com>
2023-02-14 18:06:59 +0600

    added file 11-02

---
+++ 11-02/script.js
@@ -0,0 +1,61 @@
+/********** 1 *************/
+/*
+let num = parseInt(prompt("Введите число:"));
+while (num !== 0) {
+    if (num > 0) {
+        console.log("плюс");
+    } else if (num < 0) {
+        console.log("минус");
+    }
+    num = parseInt(prompt("Введите число:"));
+}
+*/
+
+
+/********* 2 **********/
+// Реализация с помощью тернарного оператора
+/*function compareNumbers(num1, num2) {
+    return num1 > num2 ? 1 : num1 < num2 ? -1 : 0;
+}
+
+console.log(compareNumbers());*/
+
+// Реализация в виде стрелочной функции
+/*
+const compareNumbers = (num1, num2) =>
+    num1 > num2 ? 1 : num1 < num2 ? -1 : 1
+
+console.log(compareNumbers());*/
+
+
+/******** 3 **********/
+/*const numbers = [-5, 10, 5, 55, -1, 22, -4, 36, -45];
+
+const negativeNumbers = numbers.filter(num => num < 0)
+
+console.log(negativeNumbers); // [-5, -1, -4, -45]*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
