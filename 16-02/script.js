539bf001a2cc8136b0a92a86264172fac772a8e5
Пользователь <it007722007@gmail.com>
2023-02-21 18:24:06 +0600

    added file 16-02

---
+++ 16-02/script.js
@@ -0,0 +1,28 @@
+/********* 1 ********/
+let birthYear = parseInt(prompt("Введите год рождения ученика:"));
+let currentYear = parseInt(prompt("Введите текущий год:"));
+let age = currentYear - birthYear;
+console.log("Вам " + age + " лет");
+
+
+/********** 2 ********/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
