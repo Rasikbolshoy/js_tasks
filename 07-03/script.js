96836a4af58a17b61096864f867bdc47703cbf50
Пользователь <it007722007@gmail.com>
2023-03-09 18:30:10 +0600

    added file 07-03

---
+++ 07-03/script.js
@@ -0,0 +1,28 @@
+/******** 1 *******/
+/*
+var images = document.getElementsByTagName('img');
+
+
+for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function() {
+
+        var description = this.getAttribute('alt');
+
+        alert(description);
+    });
+}
+*/
+
+/******** 2 ********/
+/*const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        alert(link.href);
+    });
+});*/
+
+
+
+
+
